7dad23a9f2db212c1c1f1ab5a20f8b2efd019b67
ChaoZavitkar <55548464+ChaoZavitkar@users.noreply.github.com>
2023-10-09 15:49:07 +0200

    cviko2

--- Cviko1/main.py
+++ Cviko1/main.py
@@ -1,12 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+
+recepty = [
+    {
+        "název": "párek v rohlíku",
+        "postup": "Vemu párek, dám ho do rohlíku, dám do toho kečup"
+    },
+    {
+        "název": "gyros",
+        "postup": "vezmu Fida a uvařím ho"
+    },
+    {
+        "název": "nudle",
+        "postup": "uvařím nudle ve vodě"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/datasets")
+def datasets():
+    return render_template("datasets.html", receptar=recepty)
+
+@app.route("/recepty/<idreceptu>")
+def vrat_recept(idreceptu):
+    return recepty[int(idreceptu)]
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "GET":
+        return render_template("contact.html")
+    elif request.method == "POST":
+        nazev = request.form.get("nazev")
+        postup = request.form.get("postup")
+        recepty.append({"název": nazev, "postup": postup})
+        return redirect("datasets")
+    else:
+        return render_template("contact.html")   
 
 if __name__ == "__main__":
     app.run(debug=True)
